1eaf088a88c2154d358a0f54c3142bb66d9ce906
Jeremy Price <JeremyPriceM@gmail.com>
2019-02-27 20:32:52 -0600

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 // Blogging App
 exports.DATABASE_URL =
-    process.env.DATABASE_URL || "mongodb://my-new-user:Coolguy32@my-first-mlab-db-shard-00-00-vmc3g.mongodb.net:27017,my-first-mlab-db-shard-00-01-vmc3g.mongodb.net:27017,my-first-mlab-db-shard-00-02-vmc3g.mongodb.net:27017/test?ssl=true&replicaSet=my-first-mlab-db-shard-0&authSource=admin&retryWrites=true";
+    process.env.DATABASE_URL || "mongodb://my-new-user:Coolguy32@my-first-mlab-db-shard-00-00-vmc3g.mongodb.net:27017,my-first-mlab-db-shard-00-01-vmc3g.mongodb.net:27017,my-first-mlab-db-shard-00-02-vmc3g.mongodb.net:27017/my-first-mlab-db?ssl=true&replicaSet=my-first-mlab-db-shard-0&authSource=admin&retryWrites=true";
 exports.PORT = process.env.PORT || 8080;
