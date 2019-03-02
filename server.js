2fc1dee203c1261f29f99640a4bc08bf363b272a
Jeremy Price <JeremyPriceM@gmail.com>
2019-03-02 08:49:54 -0600

    Update

--- server.js
+++ server.js
@@ -60,14 +60,18 @@ app.post("/posts", (req, res) => {
         });
 });
 
-app.delete("/:id", (req, res) => {
+app.delete('/posts/:id', (req, res) => {
     BlogPost
         .findByIdAndRemove(req.params.id)
         .then(() => {
-            console.log(`Deleted blog post with id \`${req.params.id}\``);
-            res.status(204).end();
+            res.status(204).json({message: 'success'});
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: "something went terribly wrong"});
         });
 });
+
 
 app.put("/posts/:id", (req, res) => {
     if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
@@ -80,7 +84,7 @@ app.put("/posts/:id", (req, res) => {
 
     updateableFields.forEach(field => {
         if (field in req.body) {
-            Update[field] = req.body[field];
+            Updated[field] = req.body[field];
         }
     });
     BlogPost
@@ -88,6 +92,7 @@ app.put("/posts/:id", (req, res) => {
         .then(updatedPost => res.status(204).end())
         .catch(err => res.status(500).json({ message: "Internal server error"}));
 });
+
 app.delete("/:id", (req, res) => {
     BlogPost
     .findByIdAndRemove(req.params.id)
@@ -96,6 +101,7 @@ app.delete("/:id", (req, res) => {
         res.status(204).end();
     });
 });
+
 app.use("*", function(req, res) {
     res.status(404).json({message: "Not Found"});
 });
